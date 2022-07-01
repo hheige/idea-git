9362dc16b2d5db6fb22089337ac6d527fc7188b3
hp <1774162409@qq.com>
2022-07-01 17:54:44 +0800

    first commit

---
+++ src/main/java/com/ssmp/SpringbootssmpApplication.java
@@ -0,0 +1,13 @@
+package com.ssmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootssmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootssmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssmp/SpringbootssmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootssmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
