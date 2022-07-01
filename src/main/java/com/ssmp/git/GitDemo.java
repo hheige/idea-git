26321325b4f7e16dcac25b6130437fc50f5be62e
hp <1774162409@qq.com>
2022-07-01 18:18:26 +0800

    first commit1

--- src/main/java/com/ssmp/git/GitDemo.java
+++ src/main/java/com/ssmp/git/GitDemo.java
@@ -7,7 +7,7 @@ public class GitDemo {
         System.out.println("shabi");
         System.out.println("shabi2");
         System.out.println("shabi3");
-
+        System.out.println("shabi4");
     }
 
 }
